258d0234d2e47b91c786dacce7de1c73074a4ba0
Kamal <kamalalbatarny@gmail.com>
2022-01-30 13:03:16 -0300

    more progress

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,9 +2,7 @@ import React from "react";
 
 export const Button: React.FC = ({ children }) => {
   return (
-    <button
-      className="px-4 py-2 font-bold rounded bg-lightGray hover:text-primary-400"
-    >
+    <button className="inline-flex items-center justify-center px-4 py-2 text-white transition-all duration-300 border border-transparent rounded-md bg-buttonPrimary focus-visible:ring-buttonPrimary hover:bg-opacity-75 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2">
       {children}
     </button>
   );
